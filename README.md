1b7107caa359e3977a37015b03522dcf4592138c
pstauffer <pstauffer@confirm.ch>
2016-05-02 16:44:39 +0200

    * added sessions

--- README.md
+++ README.md
@@ -4,14 +4,14 @@
 
 ### Preparation
 
-Change the value for ``REGISTRY_URL`` in the ``docker-compose.yml`` file.
+Change the environment variable ``REGISTRY_URL`` in the ``docker-compose.yml`` file.
 ```
 REGISTRY_URL=https://docker-registry.example.com
 ```
 
 ### Basic Authentication
 
-If the docker registry has basic authentication activated, you have to set the following values in the ``docker-compose.yml`` file.
+If the docker registry has basic authentication activated, you have to set the following environment variables in the ``docker-compose.yml`` file.
 ```
 REGISTRY_AUTH=True
 REGISTRY_USER=<user>
@@ -19,7 +19,7 @@ REGISTRY_PW=<password>
 ```
 
 ### Run
-This will run the application and the proxy container, which will forward your request to the application:
+This will run the frontend application and a proxy container, which will forward your request to the application:
 
 ```bash
 docker-compose up -d
@@ -38,10 +38,11 @@ docker-compse logs
 
 * [ ] curl -i HEAD https://xxx.example.com/v2/debian/manifests/latest -> https://gist.github.com/pstauffer/ab2e1486c680539a87b5d3a069c71a21
 * [ ] Home Button
+* [ ] Navigation
 * [ ] About Page
 
 ## Informations
-* https://docs.docker.com/registry/spec/api/
+* [Docker API](https://docs.docker.com/registry/spec/api)
 
 
 ## License
--- flask-app/app.py
+++ flask-app/app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template
 
-import requests
+from requests import Session, RequestException
 import json
 import os
 
@@ -13,10 +13,6 @@ def index():
     images = j['repositories']
     return render_template('index.html', images=images)
 
-@app.route("/about")
-def about():
-    return render_template('layout.html')
-
 @app.route('/image/<path:image>')
 def tags(image):
     r = registry_request(image + '/tags/list')
@@ -27,23 +23,22 @@ def tags(image):
 def registry_request(path):
     api_url = os.environ['REGISTRY_URL'] + '/v2/' + path
 
-    # set default value to False
-    REGISTRY_AUTH = os.environ.get('REGISTRY_AUTH',False)
-
-    if REGISTRY_AUTH == "True" or REGISTRY_AUTH == "true":
-        from requests.auth import HTTPBasicAuth  
-        auth = HTTPBasicAuth(os.environ['REGISTRY_USER'], os.environ['REGISTRY_PW'])
-    else:
-        auth = None
-
     try:
-        r = requests.get(api_url, auth=auth)
+        r = s.get(api_url)
         if r.status_code == 401:
             raise Exception('Return Code was 401, Authentication required / not successful!')
         else:
             return r
-    except requests.exceptions.RequestException as e:
+    except RequestException:
         raise Exception("Problem during docker registry connection")
 
 if __name__ == "__main__":
+    s = Session()
+
+    # if not set, set authentication default value to False
+    REGISTRY_AUTH = os.environ.get('REGISTRY_AUTH',False)
+
+    if REGISTRY_AUTH == "True" or REGISTRY_AUTH == "true":
+        s.auth = (os.environ['REGISTRY_USER'], os.environ['REGISTRY_PW'])
+
     app.run(host='0.0.0.0', debug=True)
